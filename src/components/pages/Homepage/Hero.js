069dd0619394453015d4eb2420b37a7f52c24554
Lakshit Madaan <lakshitmadaan@gmail.com>
2021-08-10 23:20:24 +0530

    design updates

--- src/components/pages/Homepage/Hero.js
+++ src/components/pages/Homepage/Hero.js
@@ -6,7 +6,6 @@ import {Link} from 'react-router-dom'
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img className="back-image" src='https://images.unsplash.com/photo-1516975080664-ed2fc6a32937?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFrZXVwJTIwYXJ0aXN0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
       <h1 className="text-center">Dikshita Jindal Makeovers</h1>
       <p className="text-center">Get Stuck In A Paradise</p>
       <div className='hero-btns'>
